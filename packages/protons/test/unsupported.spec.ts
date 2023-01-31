ed392cb84847fe0be8b96c582eaca0d99d4a40d1
Alex Potsides <alex@achingbrain.net>
2023-01-31 09:27:05 +0100

    fix: throw when .proto is empty (#81)
    Refuse to compile an empty .proto file

--- packages/protons/test/unsupported.spec.ts
+++ packages/protons/test/unsupported.spec.ts
@@ -8,4 +8,9 @@ describe('unsupported', () => {
     await expect(generate('test/fixtures/proto2.proto', {})).to.eventually.be.rejected
       .with.property('message').that.contain('"required" fields are not allowed in proto3')
   })
+
+  it('should refuse to generate source from empty definition', async () => {
+    await expect(generate('test/fixtures/empty.proto', {})).to.eventually.be.rejected
+      .with.property('message').that.contain('No top-level messages found in protobuf')
+  })
 })
