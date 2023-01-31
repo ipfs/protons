76aa19882cd9fb4fa918dda6701b1f7b4ad740a4
Alex Potsides <alex@achingbrain.net>
2023-01-31 09:42:51 +0100

    chore: move bad fixtures to separate directory (#82)
    To allow regenerating good fixtures, move the bad fixtures to
    their own directory.

--- packages/protons/test/unsupported.spec.ts
+++ packages/protons/test/unsupported.spec.ts
@@ -5,12 +5,12 @@ import { generate } from '../src/index.js'
 
 describe('unsupported', () => {
   it('should refuse to generate source from proto2 definition', async () => {
-    await expect(generate('test/fixtures/proto2.proto', {})).to.eventually.be.rejected
+    await expect(generate('test/bad-fixtures/proto2.proto', {})).to.eventually.be.rejected
       .with.property('message').that.contain('"required" fields are not allowed in proto3')
   })
 
   it('should refuse to generate source from empty definition', async () => {
-    await expect(generate('test/fixtures/empty.proto', {})).to.eventually.be.rejected
+    await expect(generate('test/bad-fixtures/empty.proto', {})).to.eventually.be.rejected
       .with.property('message').that.contain('No top-level messages found in protobuf')
   })
 })
