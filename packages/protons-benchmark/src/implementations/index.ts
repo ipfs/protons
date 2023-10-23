47a4dcb5a774e6f10ed0cd52b1f5be411416a441
Felicio Mununga <felicio@users.noreply.github.com>
2023-10-23 14:52:21 +0200

    chore: benchmark Protobuf-ES (#89)
    Repo: https://github.com/bufbuild/protobuf-es
    Release: https://buf.build/blog/protobuf-es-the-protocol-buffers-typescript-javascript-runtime-we-all-deserve
    
    ---
    
    Relates to https://github.com/bufbuild/protobuf-es/issues/333
    
    ---------
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- packages/protons-benchmark/src/implementations/index.ts
+++ packages/protons-benchmark/src/implementations/index.ts
@@ -8,6 +8,7 @@ $ npx playwright-test dist/src/index.js --runner benchmark
 import { expect } from 'aegir/chai'
 import Benchmark from 'benchmark'
 import { encodeTest as pbjsEncodeTest, decodeTest as pbjsDecodeTest } from './pbjs/bench.js'
+import { Test as ProtobufEsTest } from './protobuf-es/bench_pb.js'
 import { Test as ProtobufTsTest } from './protobuf-ts/bench.js'
 import { Test as ProtobufjsTest } from './protobufjs/bench.js'
 import { Test as ProtonsTest } from './protons/bench.js'
@@ -56,6 +57,12 @@ new Benchmark.Suite()
   .add('@protobuf-ts', () => {
     const buf = ProtobufTsTest.toBinary(message)
     const result = ProtobufTsTest.fromBinary(buf)
+
+    expectDecodedCorrectly(result)
+  })
+  .add('protobuf-es', () => {
+    const buf = new ProtobufEsTest(message).toBinary()
+    const result = ProtobufEsTest.fromBinary(buf)
 
     expectDecodedCorrectly(result)
   })
