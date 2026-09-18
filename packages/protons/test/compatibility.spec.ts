f40ccf59ddb3d451f27755688f4d60a67f4ff108
achingbrain <alex@achingbrain.net>
2026-09-18 09:18:32 +0300

    chore: fix build

--- packages/protons/test/compatibility.spec.ts
+++ packages/protons/test/compatibility.spec.ts
@@ -429,6 +429,7 @@ describe('encode', () => {
   })
 
   it('does not write singular field values when defaults are omitted', () => {
+    // @ts-expect-error fields are required
     const buf = Singular.encode({})
 
     expect(buf.byteLength).to.equal(0, 'wrote default values for singular fields')
